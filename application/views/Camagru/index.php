c588f9268e0ba49697478c9a8fb6fe2a214c0c7f
vsarapin <forjobaccss4@gmail.com>
2018-06-02 20:12:00 +0300

    Camagru Page

--- application/views/Camagru/index.php
+++ application/views/Camagru/index.php
@@ -1,29 +1,34 @@
 <head>
-    <link rel="stylesheet" href="/css/styles.css">
-    <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/grids-responsive-min.css">
-    <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/menu.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
 </head>
-<div class="pure-g">
-    <div class="pure-u">
-        <div id="primary_nav_wrap" class="pure-menu pure-menu-horizontal">
-            <ul class="pure-menu-list">
-                <li class="pure-menu-item pure-menu-has-children pure-menu-allow-hover">
-                    <a href="#" id="menuLink1" class="pure-menu-link"><?=$user?></a>
-                    <ul class="pure-menu-children">
-                        <li class="pure-menu-item"><a href="/camagru" class="pure-menu-link">Главная</a></li>
-                        <li class="pure-menu-item"><a href="/authorization" class="pure-menu-link">Регистрация</a></li>
-                        <li class="pure-menu-item"><a href="/camagru/cabinet" class="pure-menu-link">Личный кабинет</a></li>
+<body style="background-color: #424242">
+<div class="container">
+<nav>
+    <div id="primary_nav_wrap" class="nav-wrapper grey darken-3"  style="padding: 15px 15px">
+        <?=$logout?>
+                    <ul>
+                        <li class="">
+                            <a href="#" id="" class=""><?=$user?></a>
+                            <ul class="left">
+                                <li class=""><a href="/camagru" class="">Главная</a></li>
+                                <li class=""><a href="/authorization" class="">Регистрация</a></li>
+                                <li class=""><a href="/camagru/cabinet" class="">Личный кабинет</a></li>
+                            </ul>
+                        </li>
                     </ul>
-                </li>
-               <?=$logout?>
-            </ul>
-        </div>
     </div>
+    </div>
+</nav>
 </div>
-<div class="pure-g" style="margin-top: 40px">
-    <div id="hideThis" class="pure-u-md-1-5">asdasd</div>
-    <div class="pure-u-md-3-5 ">asdas</div>
-    <div id="hideThisToo" class="pure-u-md-1-5 ">asdasd</div>
-</div>
-<script src="/js/camagru.js"></script> //НАДО РАЗОБРАТЬСЯ КАК СКРЫТЬ БЛОКИ БОКОВЫЕ
+</body>
+<!--<div class="container">-->
+<!--    <div class="row">-->
+<!--        <div class="col s6 m12 red lighten-2">123</div>-->
+<!--        <div class="col s6 m12 red lighten-3">123</div>-->
+<!--        <div class="col s4 offset-s4 red lighten-1">123</div>-->
+<!---->
+<!--        <div class="col s5 push-s7 red lighten-2">push</div>-->
+<!--        <div class="col s7 pull-s5 red lighten-1">pull</div>-->
+<!--    </div>-->
+<!--</div>-->
